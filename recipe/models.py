f398b9cebd347fe49b95f3a07485f0b8310ee84d
Jackson Clarke <104281508+jacksonclarke@users.noreply.github.com>
2024-11-16 14:04:43 -0700

    Added models for recipes and recipe books.

--- recipe/models.py
+++ recipe/models.py
@@ -1,39 +1,41 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_migrate
-from django.dispatch import receiver
+# from django.db.models.signals import post_migrate
+# from django.dispatch import receiver
+
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
-    class Meta:
-        # Setting the right plural name in the admin site.
-        verbose_name_plural = 'Categories'
-        # Setting the name field in alphabetical order
-        ordering = ('name',)
 
-    def __str__(self):
-        return self.name
-    
-PREDEFINED_CATEGORIES = [
-    "Appetizers",
-    "Soups and Salads",
-    "Main Dishes",
-    "Breads",
-    "Desserts",
-    "Breakfast",
-    "Holiday Favorites",
-    "Snacks",
-    "Sides",
-    "Cookies",
-    "Sauces",
-    "Not Specified",
-]
+class Recipe(models.Model):
+    title = models.CharField(max_length=255)
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    created_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="recipes"
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="recipes",
+    )
 
-@receiver(post_migrate)
-def check_categories(sender, **kwargs):
-    # Check if Category is empty
-    if Category.objects.count() == 0:
-        # If empty, populate with predefined categories.
-        for category_name in PREDEFINED_CATEGORIES:
-            Category.objects.create(name=category_name)
+
+class RecipeBook(models.Model):
+    name = models.CharField(max_length=255)
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="recipebooks"
+    )
+    shared_with = models.ManyToManyField(
+        User, related_name="shared_recipebooks", blank=True
+    )
+    recipes = models.ManyToManyField(Recipe, through="RecipeBookRecipe")
+
+
+class RecipeBookRecipe(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    recipebook = models.ForeignKey(RecipeBook, on_delete=models.CASCADE)
+    added_by = models.ForeignKey(User, on_delete=models.CASCADE)
